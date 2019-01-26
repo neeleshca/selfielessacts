39ea39775762fb8700297d1bf1eed55a167c9d49
Neelesh C A <neeleshca26@gmail.com>
2019-01-27 00:01:39 +0530

    Sorts images by descending order of modified time

--- Flask/app.py
+++ Flask/app.py
@@ -1,39 +1,40 @@
 from flask import Flask, render_template, url_for
 from inspect import getsourcefile
+import os
 from os.path import abspath
 
-import os
 
 app = Flask(__name__)
-
 PATH = abspath(getsourcefile(lambda: 0)).rsplit("/", 1)[0]
-# @app.route("/")
-# def hello_world():
-#     return render_template("home.html")
+
+
+def sorted_images(category):
+    images = os.listdir(PATH + "/static/Categories/" + category)
+    images = ["Categories/" + category + "/" + file for file in images]
+    images_time = [os.path.getmtime(PATH + "/static/" + file) for file in images]
+    sort_list = [list(a) for a in zip(images, images_time)]
+    sort_list = sorted(sort_list, reverse=True, key=lambda x: x[1])
+    images = [i[0] for i in sort_list]
+    return images
 
 
 @app.route("/")
 def images():
-    images = os.listdir(PATH + "/static/Categories/Category_0")
-    images = ["Categories/Category_0/" + file for file in images]
+    images = sorted_images("Category_0")
     return render_template("images.html", images=images)
 
 
-
 # @app.route("/user/")
-@app.route("/user/<path:thepath>")
+@app.route("/<path:thepath>")
 def show_user_profile(thepath):
-    # print("Username is ",thepath)
-    # print("Split is ",thepath.split('/'))
-    return render_template("image_single.html",image = thepath)
-    # return "User %s" % thepath
+    return render_template("image_single.html", image=thepath)
+
+
 
 @app.route("/<category>")
 def category_fun(category):
-
-    print("Category is ",category)
-    images = os.listdir(PATH + "/static/Categories/"+category)
-    images = ["Categories/"+category+"/" + file for file in images]
+    print("Category is ", category)
+    images = sorted_images(category)
     return render_template("category.html", info=category, images=images)
 
 
