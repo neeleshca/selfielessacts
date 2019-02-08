6df43a82fddcb4dcd24e551b21f9bb293efe2698
Neelesh C A <neeleshca26@gmail.com>
2019-02-08 18:52:11 +0530

    DB structure changed

--- Flask/InsertDB.py
+++ Flask/InsertDB.py
@@ -1,40 +1,64 @@
 import pymongo
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-client.drop_database("Database")
-db = client["Database"]
-actList = db["Acts"]
-categoryCount = db["Category"]
 
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+client.drop_database("database")
+db = client["database"]
+actList = db["acts"]
+categoryCount = db["category"]
 
 
 d = [
-    {"1234":{"username":"Naveen","timestamp":"23-01-2019:11-01-12",
-		   "caption":"caption text","upvotes":25,
-		   "imgb64":"TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvb",
-           "category":"Nature"
-		  }},
-    {"1235":{"username":"B","timestamp":"23-01-2019:11-01-12",
-		   "caption":"caption text","upvotes":5646,
-		   "imgb64":"TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvb",
-           "category":"Nature"
-		  }},
-    {"1236":{"username":"C","timestamp":"23-01-2019:11-01-12",
-		   "caption":"caption text","upvotes":423,
-		   "imgb64":"TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvb",
-           "category":"Nature"
-		  }},
-    {"1237":{"username":"D","timestamp":"23-01-2019:11-01-12",
-		   "caption":"caption text","upvotes":56,
-		   "imgb64":"TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvb",
-           "category":"Human"
-		  }}      
-	]
+    {
+        "act": {
+            "actID": "1234",
+            "username": "Naveen",
+            "timestamp": "23-01-2019:11-01-12",
+            "caption": "caption text",
+            "upvotes": 25,
+            "imgb64": "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvb",
+            "category": "Nature",
+        }
+    },
+    {
+        "act": {
+            "actID": "1235",
+            "username": "B",
+            "timestamp": "23-01-2019:11-01-12",
+            "caption": "caption text",
+            "upvotes": 5646,
+            "imgb64": "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvb",
+            "category": "Nature",
+        }
+    },
+    {
+        "act": {
+            "actID": "1236",
+            "username": "C",
+            "timestamp": "23-01-2019:11-01-12",
+            "caption": "caption text",
+            "upvotes": 423,
+            "imgb64": "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvb",
+            "category": "Nature",
+        }
+    },
+    {
+        "act": {
+            "actID": "1237",
+            "username": "D",
+            "timestamp": "23-01-2019:11-01-12",
+            "caption": "caption text",
+            "upvotes": 56,
+            "imgb64": "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvb",
+            "category": "Human",
+        }
+    },
+]
 
 x = actList.insert_many(d)
 
 cat = [
-    {"Nature":100},
-    {"Humans":50},
-    {"Others":25}
+    {"category": {"name": "Nature", "count": 100}},
+    {"category": {"name": "Humans", "count": 50}},
+    {"category": {"name": "Other", "count": 25}},
 ]
 y = categoryCount.insert_many(cat)
