03db2e5fbd13718704499cbcc7083375e4527092
midsterx <mimosk25@gmail.com>
2019-04-19 11:10:19 +0530

    Threaded Fault Tolerance

--- Project/Orchestrator/faulttolerance_temp.py
+++ Project/Orchestrator/faulttolerance_temp.py
@@ -2,28 +2,35 @@ import requests
 import docker
 import time
 
-portids = dict()
+portIds = dict()
+
+def containerCreate(oldId, samePort):
+        global portIds
+        del portIds[samePort]
+        a = client.containers.get(oldId)
+        a.stop()
+        a.remove()
+        b = client.containers.run('acts',ports={'80/tcp': int(samePort)},network="network1",environment={"TEAM_ID":"CC_208_222_223_236"},name=str(samePort)+"_acts",detach = True)
+        portIds[samePort] = b.id
+        print("Removed identifier = "+str(oldId))
+        print("New identifier = "+str(b.id))
+
 client = docker.from_env()
 
 for i in client.containers.list():
     if (i.name[5:9] == "acts"):
-        portids[i.name[0:4]] = i.id
+        portIds[i.name[0:4]] = i.id
 
 while (True):
-    for port, iden in portids.items():
+    for port in list(portIds):
         try:
             response = requests.get("http://127.0.0.1:" + str(port) + "/api/v1/_health")
         except:
             continue
         # unhealthy container detected
         if 'response' in locals() and response.status_code == 500:
-            a = client.containers.get(iden)
-            a.stop()
-            a.remove()
-            b = client.containers.run('acts',ports={'80/tcp': int(port)},network="network1",environment={"TEAM_ID":"CC_208_222_223_236"},name=str(port)+"_acts",detach = True)
-            portids[port] = b.id
-            print("Removed identifier = " + str(iden))
-            print("New identifier = " + str(b.id))
+            containerCreateThread = threading.Thread(target = containerCreate, args = (portIds[port], port))
+            containerCreateThread.start()
     time.sleep(1)
 
 
